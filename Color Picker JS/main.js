acdb87461ca6f6ba0d60f4176ec4a7e2b7c8ffbe
Asad <asad200ana@gmail.com>
2022-08-03 11:09:37 +0530

    final

--- Color Picker JS/main.js
+++ Color Picker JS/main.js
@@ -1,4 +1,4 @@
-class Color {
+class Colour {
   constructor(hex, element) {
     this.hex = hex;
     this.element = element;
@@ -8,18 +8,18 @@ class Color {
   setHex(hex) {
     this.hex = hex;
     this.element.style.backgroundColor = hex;
-    this.element.querySelector(".color-inpput").value = hex;
+    this.element.querySelector(".colour-input").value = hex;
   }
 
   setLocked(locked) {
     this.locked = locked;
 
     if (locked) {
-      this.element.classList.add("locked");
+      this.element.querySelector(".lock-toggle").classList.add("is-locked");
 
       this.element.querySelector("img").src = "icons/lock-closed.svg";
     } else {
-      this.element.classList.remove("locked");
+      this.element.querySelector(".lock-toggle").classList.remove("is-locked");
 
       this.element.querySelector("img").src = "icons/lock-open.svg";
     }
@@ -34,25 +34,59 @@ class Color {
       return;
     }
 
-    const char = "0123456789ABCDEF";
-
-    let hex = "#";
-
+    const chars = "0123456789ABCDEF";
+    let color = "#";
     for (let i = 0; i < 6; i++) {
-      hex += char[Math.floor(Math.random() * 16)];
+      color += chars[Math.floor(Math.random() * 16)];
     }
+
+    this.setHex(color);
   }
 
   copyToClipboard() {
-    const input = this.element.querySelector("color-input");
+    const input = this.element.querySelector(".colour-input");
     input.select();
     document.execCommand("copy");
     input.blur();
 
     this.element.classList.add("copied");
-
     setTimeout(() => {
       this.element.classList.remove("copied");
     }, 1000);
   }
 }
+
+const colour_elements = document.querySelectorAll(".colours .colour");
+
+const colours = [];
+
+for (let i = 0; i < colour_elements.length; i++) {
+  const colour_element = colour_elements[i];
+
+  const input = colour_element.querySelector(".colour-input");
+  const lock_toggle = colour_element.querySelector(".lock-toggle");
+  const copy_btn = colour_element.querySelector(".copy-hex");
+
+  const hex = input.value;
+
+  const colour = new Colour(hex, colour_element);
+
+  input.addEventListener("input", (e) => colour.setHex(e.target.value));
+  lock_toggle.addEventListener("click", () => colour.toggleLocked());
+  copy_btn.addEventListener("click", () => colour.copyToClipboard());
+
+  colour.generateHex();
+  colours.push(colour);
+}
+
+document.querySelector(".generator-button").addEventListener("click", () => {
+  for (let i = 0; i < colours.length; i++) {
+    colours[i].generateHex();
+  }
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.code.toLowerCase() === "space") {
+    document.querySelector(".generator-button").click();
+  }
+});
